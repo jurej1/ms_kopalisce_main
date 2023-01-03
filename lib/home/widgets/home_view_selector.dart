5d3b51ee93a93e9b40d75c207ddb0832c4af6cdb
Jure <jure.04jj@gmail.com>
2023-01-03 13:16:15 +0100

    cleanup

--- lib/home/widgets/home_view_selector.dart
+++ lib/home/widgets/home_view_selector.dart
@@ -3,32 +3,73 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../home.dart';
 
-class HomeViewSelector extends StatelessWidget {
+class HomeViewSelector extends StatelessWidget implements PreferredSizeWidget {
   const HomeViewSelector({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return BlocBuilder<HomeViewSelectorCubit, HomeViewSelectorState>(
       builder: (context, state) {
-        return BottomNavigationBar(
-          currentIndex: HomeViewSelectorState.values.indexOf(state),
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.grey,
-          items: HomeViewSelectorState.values
-              .map(
-                (e) => BottomNavigationBarItem(
-                  icon: Icon(e.mapStateToIcon()),
-                  label: '',
-                ),
-              )
-              .toList(),
-          onTap: (index) {
-            BlocProvider.of<HomeViewSelectorCubit>(context).valuUpdatedIndex(index);
-          },
+        // return BottomNavigationBar(
+        //   currentIndex: HomeViewSelectorState.values.indexOf(state),
+        //   showSelectedLabels: false,
+        //   showUnselectedLabels: false,
+        //   selectedItemColor: Colors.blue,
+        //   unselectedItemColor: Colors.grey,
+        //   items: HomeViewSelectorState.values
+        //       .map(
+        //         (e) => BottomNavigationBarItem(
+        //           icon: Icon(e.mapStateToIcon()),
+        //           label: '',
+        //         ),
+        //       )
+        //       .toList(),
+        //   onTap: (index) {
+        //     BlocProvider.of<HomeViewSelectorCubit>(context).valuUpdatedIndex(index);
+        //   },
+        // );
+
+        return ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: kBottomNavigationBarHeight,
+            maxWidth: size.width,
+            minHeight: kBottomNavigationBarHeight,
+            minWidth: size.width,
+          ),
+          child: Material(
+            color: Colors.blue,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: HomeViewSelectorState.values.map(
+                (e) {
+                  final bool isSelected = state == e;
+
+                  return Transform.scale(
+                    scale: isSelected ? 1.1 : 0.9,
+                    child: IconButton(
+                      color: isSelected ? Colors.white : Colors.white.withOpacity(0.7),
+                      icon: Icon(e.mapStateToIcon()),
+                      onPressed: () {
+                        BlocProvider.of<HomeViewSelectorCubit>(context).valueUpdatedEnum(e);
+                      },
+                    ),
+                  );
+                },
+              ).toList(),
+            ),
+          ),
         );
       },
     );
   }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kBottomNavigationBarHeight);
 }
