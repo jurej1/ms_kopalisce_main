a27cdd95a3f2fe361fca75d7cb8c3e103dc4ccd5
Jure <jure.04jj@gmail.com>
2023-01-07 11:24:49 +0100

    cleanup

--- lib/quiz/view/quiz_home_view.dart
+++ lib/quiz/view/quiz_home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_repository/quiz_repository.dart';
 
 import '../../home/home.dart';
 import 'view.dart';
@@ -16,14 +17,32 @@ class QuizHomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            //Start the Quiz
-            Navigator.of(context).push(QuizView.route(context));
-          },
-          child: const Text('Start Quiz'),
+      body: SizedBox(
+        height: size.height,
+        width: size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              fit: BoxFit.fill,
+              height: size.height * 0.4,
+              QuizImages.sobotaSvica,
+              package: 'quiz_repository',
+            ),
+            const SizedBox(height: 20),
+            SizedBox(
+              width: size.width * 0.4,
+              child: ElevatedButton(
+                onPressed: () {
+                  //Start the Quiz
+                  Navigator.of(context).push(QuizView.route(context));
+                },
+                child: const Text('Start Quiz'),
+              ),
+            ),
+          ],
         ),
       ),
       bottomNavigationBar: const HomeViewSelector(),
