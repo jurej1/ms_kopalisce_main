794d961cfa4e5b1fc49aef4fae5661da36a41ba9
Jure <jure.04jj@gmail.com>
2023-01-15 12:37:17 +0100

    splash screen

--- lib/app.dart
+++ lib/app.dart
@@ -107,16 +107,18 @@ class _AppState extends State<App> {
             primarySwatch: Colors.blue,
           ),
           builder: (context, child) {
-            return BlocListener<AuthenticationBloc, AuthenticationState>(
-              listener: ((context, state) {
-                if (state.status == AuthenticationStatus.unauthenticated) {
-                  _navigatorState.currentState!.pushReplacement(LoginView.route(context));
-                } else if (state.status == AuthenticationStatus.authenticated) {
-                  _navigatorState.currentState!.pushReplacement(HomeView.route(context));
-                }
-              }),
-              child: child!,
-            );
+            // return BlocListener<AuthenticationBloc, AuthenticationState>(
+            //   listener: ((context, state) {
+            //     if (state.status == AuthenticationStatus.unauthenticated) {
+            //       _navigatorState.currentState!.pushReplacement(LoginView.route(context));
+            //     } else if (state.status == AuthenticationStatus.authenticated) {
+            //       _navigatorState.currentState!.pushReplacement(HomeView.route(context));
+            //     }
+            //   }),
+            //   child: child!,
+            // );
+
+            return child!;
           },
           home: _SplashScreen(),
         ),
@@ -128,9 +130,29 @@ class _AppState extends State<App> {
 class _SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
       body: Center(
-        child: Text('This is splash screen'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/sobota_svica.png',
+              height: size.width * 0.55,
+            ),
+            const SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.3,
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: const LinearProgressIndicator(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
