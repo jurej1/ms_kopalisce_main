b52fb4a8389a292b8d9e1a1809319b868a053e7a
Jure <jure.04jj@gmail.com>
2023-01-22 21:09:55 +0100

    cleanup

--- lib/prices/view/store_view.dart
+++ lib/prices/view/store_view.dart
@@ -1,22 +1,54 @@
 import 'package:flutter/material.dart';
-import 'package:ms_kopalisce_main/app.dart';
+import 'package:price_repository/price_repository.dart';
 
 class StoreView extends StatelessWidget {
-  const StoreView({Key? key}) : super(key: key);
+  StoreView({Key? key}) : super(key: key);
 
   static route() {
     return MaterialPageRoute(
       builder: (context) {
-        return const StoreView();
+        return StoreView();
       },
     );
   }
+
+  final List<StoreItem> items = RudyRepository.allStoreItems;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Rudijeva trgovina'),
+      ),
+      body: GridView.builder(
+        padding: const EdgeInsets.all(10),
+        shrinkWrap: true,
+        itemCount: items.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return GridTile(
+            key: ValueKey(item),
+            child: Container(
+              child: const Text('hello'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: AssetImage(
+                    item.imagesPaths.first,
+                    package: 'price_repository',
+                  ),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
